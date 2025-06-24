e9d2d7911a509975df679f0b707519b0540b042d
LuisPascoal32 <ls_pascoal@live.com.pt>
2025-06-24 21:38:05 +0100

    Poc (#4)
    * Checklist and styles poc
    
    * Search list, image gallery design update, theming
    
    * Update to use component
    
    * Update search and icons
    
    * Dialog poc, updating some styles
    
    * Fix zoom mobile
    
    ---------
    
    Co-authored-by: RMB <rui.mandimbraga@gmail.com>

--- src/app/plate/plate.ts
+++ src/app/plate/plate.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog';
 
 @Component({
   selector: 'app-plate',
@@ -8,4 +10,16 @@ import { Component, Input } from '@angular/core';
 export class PlateComponent {
   @Input() topValue: string = "129";
   @Input() bottomValue: string = "1203";
+
+  private readonly dialog = inject(MatDialog);
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: {},
+      height: "100%",
+      width: "100%",
+      maxWidth: "100%",
+      maxHeight: "100%"
+    })
+  }
 }
