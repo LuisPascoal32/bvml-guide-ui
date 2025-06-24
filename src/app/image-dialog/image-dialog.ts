af1896ab0c683f2050c64c95427ca2efd8738c61
Luis Pascoal <luispascoal@macbookpro.lan>
2025-06-24 14:31:24 +0100

    Add fullscreen when img click

--- src/app/image-dialog/image-dialog.ts
+++ src/app/image-dialog/image-dialog.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-image-dialog',
   templateUrl: './image-dialog.html',
-  styleUrls: ['./image-dialog.scss']
+  styleUrls: ['./image-dialog.scss'],
+  imports: [CommonModule]
 })
 export class ImageDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { imageUrl: string }) {}
+  isFullscreen = true;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { imageUrl: string }) { }
+
+  openFullscreen() {
+    this.isFullscreen = true;
+  }
+
+  closeFullscreen() {
+    this.isFullscreen = false;
+  }
 }
+
+
