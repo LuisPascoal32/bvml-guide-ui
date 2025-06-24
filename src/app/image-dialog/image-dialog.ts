462d7d351b2d989dbf1bca911061c38b8e70d21c
unknown <ls_pascoal@live.com.pt>
2025-06-24 20:42:16 +0100

    Fix zoom in img

--- src/app/image-dialog/image-dialog.ts
+++ src/app/image-dialog/image-dialog.ts
@@ -1,31 +1,21 @@
-import { AfterViewInit, Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import panzoom from 'panzoom';
 
 @Component({
   selector: 'app-image-dialog',
-  standalone: true,
   templateUrl: './image-dialog.html',
   styleUrls: ['./image-dialog.scss'],
+  imports: [CommonModule]
 })
-export class ImageDialogComponent implements AfterViewInit {
-  @ViewChild('zoomContainer', { static: true }) zoomContainer!: ElementRef;
-
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: { imageUrl: string },
+export class ImageDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { imageUrl: string },
     private dialogRef: MatDialogRef<ImageDialogComponent>
-  ) {}
-
-  ngAfterViewInit(): void {
-    panzoom(this.zoomContainer.nativeElement, {
-      maxZoom: 5,
-      minZoom: 1,
-      bounds: true,
-      boundsPadding: 0.1,
-    });
-  }
-
+  ) { }
   onBackdropClick(): void {
     this.dialogRef.close();
   }
 }
+
+
+
