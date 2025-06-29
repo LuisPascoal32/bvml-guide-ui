79c6e547cf0925a0a57b960b38c71778ec4e6ce3
LuisPascoal32 <ls_pascoal@live.com.pt>
2025-06-29 19:00:31 +0100

    subgroup (#8)
    Co-authored-by: RMB <rui.mandimbraga@gmail.com>

--- src/app/image-gallery/image.service.ts
+++ src/app/image-gallery/image.service.ts
@@ -3,12 +3,17 @@ export interface Image {
   path: string;
 }
 
+export interface ImageSubGroup {
+  label: string;
+  images: Image[];
+}
+
 export interface ImageGroup {
   icon?: string;
   label: string;
   route: string;
   expanded?: boolean;
-  images?: Image[];
+  images?: ImageSubGroup[];
   children?: ImageGroup[];
 }
 
@@ -17,8 +22,7 @@ export const IMAGE_GROUPS: ImageGroup[] = [
     icon: 'assets/emt/emt.ico',
     label: 'Pré-Hospitalar',
     route: 'pre-hospitalar',
-    images: [
-    ],
+    images: [],
   },
   {
     icon: 'assets/ui/ui.ico',
@@ -30,17 +34,44 @@ export const IMAGE_GROUPS: ImageGroup[] = [
         label: 'Equipamento',
         route: 'equipment',
         images: [
-          { title: 'Câmara Térmica', path: 'assets/ui/equipment/camaraTermica.png' },
-          { title: 'Multi Gás', path: 'assets/ui/equipment/multiGas.png' },
-          { title: 'Multi Gás Alarme', path: 'assets/ui/equipment/multiGasAlarme.png' },
-          { title: 'Multi Gás Valores Ref.', path: 'assets/ui/equipment/multiGasReferencia.png' }
+          {
+            label: 'Camara termica',
+            images: [
+              {
+                title: 'Câmara Térmica',
+                path: 'assets/ui/equipment/camaraTermica.png',
+              },
+            ],
+          },
+          {
+            label: 'Gáses',
+            images: [
+              {
+                title: 'Multi Gás',
+                path: 'assets/ui/equipment/multiGas.png'
+              },
+              {
+                title: 'Multi Gás Alarme',
+                path: 'assets/ui/equipment/multiGasAlarme.png',
+              },
+              {
+                title: 'Multi Gás Valores Ref.',
+                path: 'assets/ui/equipment/multiGasReferencia.png',
+              },
+            ],
+          },
         ],
       },
       {
         label: 'Fugas Gás',
         route: 'gasLeak',
         expanded: false,
-        images: [{ title: 'Fuga Gás (Conduta)', path: 'assets/gasLeak/fugaGasConduta.png' }],
+        images: [
+          {
+            title: 'Fuga Gás (Conduta)',
+            path: 'assets/gasLeak/fugaGasConduta.png',
+          },
+        ],
       },
     ],
   },
@@ -55,46 +86,65 @@ export const IMAGE_GROUPS: ImageGroup[] = [
         route: 'silosleixoes',
         images: [
           { title: 'AVISO!', path: 'assets/ppi/silos/silosWarning.png' },
-          { title: 'Emergências Médicas(1)', path: 'assets/ppi/silos/silosEmergenciasMedicas1.png' },
-          { title: 'Emergências Médicas(2)', path: 'assets/ppi/silos/silosEmergenciasMedicas2.png' },
-          { title: 'Espaços Confinados(1)', path: 'assets/ppi/silos/silosEspacosConfinados1.png' },
-          { title: 'Espaços Confinados(2)', path: 'assets/ppi/silos/silosEspacosConfinados2.png' },
-          { title: 'Espaços Confinados(3)', path: 'assets/ppi/silos/silosEspacosConfinados3.png' },
-          { title: 'Incêndio Interior(1)', path: 'assets/ppi/silos/silosIncendioInterior1.png' },
-          { title: 'Incêndio Interior(2)', path: 'assets/ppi/silos/silosIncendioInterior2.png' },
-          { title: 'Incêndio Fosfina', path: 'assets/ppi/silos/silosIncendioFosfina.png' },
+          {
+            title: 'Emergências Médicas(1)',
+            path: 'assets/ppi/silos/silosEmergenciasMedicas1.png',
+          },
+          {
+            title: 'Emergências Médicas(2)',
+            path: 'assets/ppi/silos/silosEmergenciasMedicas2.png',
+          },
+          {
+            title: 'Espaços Confinados(1)',
+            path: 'assets/ppi/silos/silosEspacosConfinados1.png',
+          },
+          {
+            title: 'Espaços Confinados(2)',
+            path: 'assets/ppi/silos/silosEspacosConfinados2.png',
+          },
+          {
+            title: 'Espaços Confinados(3)',
+            path: 'assets/ppi/silos/silosEspacosConfinados3.png',
+          },
+          {
+            title: 'Incêndio Interior(1)',
+            path: 'assets/ppi/silos/silosIncendioInterior1.png',
+          },
+          {
+            title: 'Incêndio Interior(2)',
+            path: 'assets/ppi/silos/silosIncendioInterior2.png',
+          },
+          {
+            title: 'Incêndio Fosfina',
+            path: 'assets/ppi/silos/silosIncendioFosfina.png',
+          },
         ],
       },
       {
         label: 'IKEA',
         route: 'ikea',
-        images: [
-        ],
+        images: [],
       },
       {
         label: 'Mar Shopping',
         route: 'marshopping',
-        images: [
-        ],
+        images: [],
       },
       {
         label: 'Resende',
         route: 'resende',
-        images: [
-        ],
+        images: [],
       },
       {
         label: 'Pergás',
         route: 'pergas',
-        images: [
-        ],
+        images: [],
       },
       {
         label: 'Padre Ângelo',
         route: 'padreangelo',
-        images: [
-        ],
-      }
+        images: [],
+      },
     ],
   },
   {
@@ -108,22 +158,23 @@ export const IMAGE_GROUPS: ImageGroup[] = [
         label: 'Elétricos',
         route: 'eletrics',
         images: [
-          { title: 'Baterias Lítio', path: 'assets/specific/eletricvehicles/eletricVehicles.png' },
+          {
+            title: 'Baterias Lítio',
+            path: 'assets/specific/eletricvehicles/eletricVehicles.png',
+          },
         ],
       },
-            {
+      {
         icon: '',
         label: 'Hidrogénio',
         route: 'hidro',
-        images: [
-        ],
+        images: [],
       },
-            {
+      {
         icon: '',
         label: 'Metro',
         route: 'metro',
-        images: [
-        ],
+        images: [],
       },
     ],
   },
@@ -138,15 +189,25 @@ export const IMAGE_GROUPS: ImageGroup[] = [
         label: 'Intervenção',
         route: 'intervention',
         images: [
-          { title: '!!REGRAS!!', path: 'assets/specific/hazmat/hazmatRegras.png' },
-          { title: 'Intervenção(1)', path: 'assets/specific/hazmat/intervencao1Hazmat.png' },
-          { title: 'Intervenção(2)', path: 'assets/specific/hazmat/intervencao2Hazmat.png' }],
+          {
+            title: '!!REGRAS!!',
+            path: 'assets/specific/hazmat/hazmatRegras.png',
+          },
+          {
+            title: 'Intervenção(1)',
+            path: 'assets/specific/hazmat/intervencao1Hazmat.png',
+          },
+          {
+            title: 'Intervenção(2)',
+            path: 'assets/specific/hazmat/intervencao2Hazmat.png',
+          },
+        ],
       },
       {
         icon: 'assets/hazmat/hazmat.ico',
         label: 'Nr ONU',
-        route: 'onu'
-      }
-    ]
-  }
+        route: 'onu',
+      },
+    ],
+  },
 ];
